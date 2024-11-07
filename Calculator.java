b4ae36df0f9c4339a0069745e9f105a7f4814827
mariemt26 <Meriemtouati80@gmail.com>
2024-11-07 00:00:44 -0500

    Update Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main(String[] args){
+
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your expression (operators included AND space out each term): ");
+
+        String expression = scanner.nextLine();
+
+        String[] operatorString = expression.split(" ");
+
+
+        for(int i = 0; i < operatorString.length; i++){
+            
+
+        }
+
+
+
+
+
+	}
+}
+
+
