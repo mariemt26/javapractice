1bd87f2da417c68936dae5bed5cf1da700785a67
mariemt26 <Meriemtouati80@gmail.com>
2024-11-10 22:07:56 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Calculator{
     public static void main(String[] args){
 
-		Scanner scanner = new Scanner(System.in);
+		    Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your expression (operators included AND space out each term): ");
 
         String expression = scanner.nextLine();
@@ -11,11 +11,25 @@ public class Calculator{
         String[] operatorString = expression.split(" ");
 
 
+        int result = Integer.parseInt(operatorString[0]);
         for(int i = 0; i < operatorString.length; i++){
+
+          if(operatorString[i].equals("+")){
+            result += Integer.parseInt(operatorString[i + 1]);
+          }
+          if(operatorString[i].equals("-")){
+            result -= Integer.parseInt(operatorString[i + 1]);
+          }
+          if(operatorString[i].equals("*")){
+            result *= Integer.parseInt(operatorString[i + 1]);
+          }
+          if(operatorString[i].equals("/")){
+            result /= Integer.parseInt(operatorString[i + 1]);
+          }
             
 
         }
-
+        System.out.println(result);
 
 
 
