cabc25c4ffbe5b18e524b19168f399d32b240b33
mariemt26 <Meriemtouati80@gmail.com>
2024-12-14 23:33:51 -0500

    Update Fizzbuzz.java

--- Fizzbuzz/Fizzbuzz.java
+++ Fizzbuzz/Fizzbuzz.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 public class Fizzbuzz {
-    
+
     System.out.print("Number: ");
     Scanner scanner = new Scanner(System.in);
 
     int userNum = scanner.nextInt();
+
+    if(userNum%)
 
 }
