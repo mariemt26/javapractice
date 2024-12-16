3448ac4d8cab39a48ccb03ea16e6ce8f5cdd15e3
mariemt26 <Meriemtouati80@gmail.com>
2024-12-16 15:09:35 -0500

    Update to FizzBuzz.java
    So I'm stupid and forgot to put everything inside a method.....forget that last commit ever happened.

--- Fizzbuzz/Fizzbuzz.java
+++ Fizzbuzz/Fizzbuzz.java
@@ -2,29 +2,31 @@ import java.util.Scanner;
 
 public class Fizzbuzz {
 
-    System.out.print("Number: ");
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
 
-    int userNum = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Number: ");
 
-    if(userNum%5==0)
-    {
-        System.out.println("Fizz");
-    }
+        int userNum = scanner.nextInt();
 
-    else if(userNum%3==0)
-    {
-        System.out.println("Buzz");
-    }
+        if (userNum % 5 == 0) {
+            System.out.println("Fizz");
+        }
 
-    else if(userNum%5==0&&userNum%3==0)
-    {
-        System.out.println("Fizzbuzz");
-    }
+        else if (userNum % 3 == 0) {
+            System.out.println("Buzz");
+        }
 
-    else
-    {
-        System.out.println(userNum);
+        else if (userNum % 5 == 0 && userNum % 3 == 0) {
+            System.out.println("Fizzbuzz");
+        }
+
+        else {
+            System.out.println(userNum);
+        }
+
+        scanner.close();
+
     }
 
 }
