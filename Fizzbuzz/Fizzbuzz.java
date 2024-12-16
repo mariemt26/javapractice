9f740004d281a9f4e07f03010f12c865933b1b5a
mariemt26 <Meriemtouati80@gmail.com>
2024-12-16 15:45:04 -0500

    Final FizzBuzz Update
    Finally got all the conditions working. In the future, I will go back to this and make the more complex implementation.

--- Fizzbuzz/Fizzbuzz.java
+++ Fizzbuzz/Fizzbuzz.java
@@ -5,20 +5,20 @@ public class Fizzbuzz {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Number: ");
+        System.out.print("Number: ");
 
         int userNum = scanner.nextInt();
 
-        if (userNum % 5 == 0) {
+        if (userNum % 5 == 0 && userNum % 3 == 0) {
+            System.out.println("Fizzbuzz");
+        }
+
+        else if (userNum % 5 == 0) {
             System.out.println("Fizz");
         }
 
         else if (userNum % 3 == 0) {
             System.out.println("Buzz");
-        }
-
-        else if (userNum % 5 == 0 && userNum % 3 == 0) {
-            System.out.println("Fizzbuzz");
         }
 
         else {
